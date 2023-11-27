8237195ae08f401e5de2c54fa01bc447082283fc
LikithaPemmireddi <119496519+LikithaPemmireddi@users.noreply.github.com>
2023-11-27 20:59:34 +0530

    Add files via upload

---
+++ Gateway Server/Gateway-Server/src/test/java/Gateway/Server/Gateway/Server/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Gateway.Server.Gateway.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
